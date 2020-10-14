8d8b0fafd3c0e1c39b7f7ca8eb0b98037c75b774
XavierQ <xquintana93@gmail.com>
2020-10-14 16:32:03 +0200

    canvis en els asserts perque donin correcte les funcions

--- src/test/java/edu/udg/caes/ExerciseIntroTestingTest.java
+++ src/test/java/edu/udg/caes/ExerciseIntroTestingTest.java
@@ -29,4 +29,27 @@ public class ExerciseIntroTestingTest {
         final int result = ExerciseIntroTesting.oddOrPos(new int []{-3, -2, 0, 1, 4});
         assertEquals(result, 3);
     }
+    @Test
+    public void testFindLast2(){
+        final int result = ExerciseIntroTesting.findLast(new int []{1,2, 3, 5}, 2);
+        assertEquals(result, 1);
+    }
+
+    @Test
+    public void testLastZero2(){
+        final int result = ExerciseIntroTesting.lastZero(new int []{0, 1, 0});
+        assertEquals(result, 0);
+    }
+
+    @Test
+    public void testCountPositive2(){
+        final int result = ExerciseIntroTesting.countPositive(new int []{-4, 2, 2});
+        assertEquals(result, 2);
+    }
+
+    @Test
+    public void testOddOrPos2(){
+        final int result = ExerciseIntroTesting.oddOrPos(new int []{1,2, 3});
+        assertEquals(result, 3);
+    }
 }
